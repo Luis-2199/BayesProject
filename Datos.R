98c024d985dccd0a64375b6bac04ce77a1059a31
luise <luis.e.pimentel@ciencias.unam.mx>
2021-07-25 02:59:31 -0500

    más estructura

--- Datos.R
+++ Datos.R
@@ -12,14 +12,16 @@ Data_raw_Sec67 <- read_csv("sec67.csv")
 
 glimpse(Data_raw_Sec67)
 
-Data <- inner_join(Data_raw_Sec3, Data_raw_Sec45) %>%  
-        inner_join(Data_raw_Sec67) %>% 
-        select(-ID_PER, -CVE_ENT, -CVE_MUN, -AP6_1_2) %>% 
-        filter(AP7_3_13 != 9, AP7_3_14 != 9) %>% 
-        mutate(AP7_4_13 = replace_na( AP7_4_13, 0), AP7_4_14 = replace_na(AP7_4_14, 0)) %>% 
-        select(-AP7_3_13, -AP7_3_14) %>%  
-        mutate(Num_Inc = AP7_4_13 + AP7_4_14) %>% 
-        select(-AP7_4_13, -AP7_4_14)
+Data_join <- inner_join(Data_raw_Sec3, Data_raw_Sec45) %>%  
+              inner_join(Data_raw_Sec67) 
+
+Data <- Data_join %>% 
+          select(-ID_PER, -CVE_ENT, -CVE_MUN, -AP6_1_2) %>% 
+          filter(AP7_3_13 != 9, AP7_3_14 != 9) %>% 
+          mutate(AP7_4_13 = replace_na( AP7_4_13, 0), AP7_4_14 = replace_na(AP7_4_14, 0)) %>% 
+          select(-AP7_3_13, -AP7_3_14) %>%  
+          mutate(Num_Inc = AP7_4_13 + AP7_4_14) %>% 
+          select(-AP7_4_13, -AP7_4_14)
 
 Data_per_est <- Data %>% 
                   group_by(NOM_ENT) %>% 
@@ -38,7 +40,6 @@ Data_Izt <- Data_cdmx %>%
 Data_per_mun <-  Data_cdmx %>% 
                   group_by(NOM_MUN) %>% 
                   summarise(Sum_Num = sum(Num_Inc))
-
 
 
 
