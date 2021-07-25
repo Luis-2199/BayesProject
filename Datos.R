a7f89ecf916cd38a5041cfc9a554b728e4d9f7c0
Edson Fernández <edsonjfg98@ciencias.unam.mx>
2021-07-25 18:37:06 -0500

    Última Versión

--- Datos.R
+++ Datos.R
@@ -14,30 +14,24 @@ glimpse(Data_raw_Sec67)
 
 Data <- inner_join(Data_raw_Sec3, Data_raw_Sec45) %>%  
         inner_join(Data_raw_Sec67) %>% 
-        select(-ID_PER, -CVE_ENT, -CVE_MUN, -AP6_1_2) %>% 
-        filter(AP7_3_05 != 9,AP7_3_06 != 9,AP7_3_07 != 9,AP7_3_08 != 9,AP7_3_09 != 9,
-               AP7_3_10 != 9,AP7_3_11 != 9,AP7_3_12 != 9,AP7_3_13 != 9,AP7_3_14 != 9,AP7_3_15 != 9) %>% 
-        mutate(AP7_4_05 = replace_na( AP7_4_05, 0), AP7_4_06 = replace_na(AP7_4_06, 0),AP7_4_07 = replace_na( AP7_4_07, 0),
-               AP7_4_08 = replace_na( AP7_4_08, 0),AP7_4_09 = replace_na( AP7_4_09, 0),AP7_4_10 = replace_na( AP7_4_10, 0),
-               AP7_4_11 = replace_na( AP7_4_11, 0),AP7_4_12 = replace_na( AP7_4_12, 0),AP7_4_13 = replace_na( AP7_4_13, 0),
-               AP7_4_14 = replace_na( AP7_4_14, 0),AP7_4_15 = replace_na( AP7_4_15, 0)) %>% 
-        select(-AP7_3_05,-AP7_3_06,-AP7_3_07,-AP7_3_08,-AP7_3_09,-AP7_3_10,-AP7_3_11,-AP7_3_12,-AP7_3_13,-AP7_3_14,-AP7_3_15) 
-        #mutate(Num_Inc = AP7_4_13 + AP7_4_14) %>% 
-        #select(-AP7_4_13, -AP7_4_14)
+        select(-ID_PER, -CVE_ENT, -CVE_MUN, -AP6_1_2,-AP7_3_07,-AP7_3_08,-AP7_3_09,-AP7_3_10,-AP7_3_11,-AP7_3_12,-AP7_3_15,
+               -AP7_4_07,-AP7_4_08,-AP7_4_09,-AP7_4_10,-AP7_4_11,-AP7_4_12,-AP7_4_15) %>% 
+        filter(AP7_3_05 != 9,AP7_3_06 != 9,AP7_3_13 != 9,AP7_3_14 != 9) %>% 
+        mutate(AP7_4_05 = replace_na( AP7_4_05, 0), AP7_4_06 = replace_na(AP7_4_06, 0),AP7_4_13 = replace_na( AP7_4_13, 0),
+               AP7_4_14 = replace_na( AP7_4_14, 0)) %>% 
+        select(-AP7_3_05,-AP7_3_06,-AP7_3_13,-AP7_3_14) %>%
+        mutate(Vic_Rob_As = AP7_4_06 + AP7_4_05,Vic_Sex = AP7_4_13 + AP7_4_14) %>% 
+        select(-AP7_4_13, -AP7_4_14,-AP7_4_05,-AP7_4_06)
 
-Data_per_est <- Data %>% 
+Data_CDMX <- Data %>% 
   filter(NOM_ENT == "Ciudad de Mexico") %>%
-  group_by(NOM_MUN) %>%
-  summarise(Robo_Asalto = sum(AP7_4_05),Otro_Robo = sum(AP7_4_06),Fraude_b = sum(AP7_4_07),Fraude_con = sum(AP7_4_08),
-            Extorsion = sum(AP7_4_09),Amenaza = sum(AP7_4_10),Lesion = sum(AP7_4_11),Secuestro = sum(AP7_4_12),Host = sum(AP7_4_13),
-            Violacion = sum(AP7_4_14),Otros = sum(AP7_4_15))
+  mutate(Sit_Lab_Act =AP3_8, Pos_OCup =replace_na( AP3_10, 0), Imp_Seg = AP4_2_05, Seg_Loc=AP4_3_1, Seg_Mun =AP4_3_2, Alum =AP4_8_1,
+         Agua=AP4_8_2,Pandill=AP4_8_4,Robos=AP4_8_5,Del_Esc=AP4_8_6,Mas_Op_Del=AP4_5_08,Mas_Pat_Vil=AP4_5_10, Vehic=AP6_1_1) %>%
+  select(-AP3_8,-AP3_10,-AP4_2_05,-AP4_3_1,-AP4_3_2,-AP4_8_1,-AP4_8_2,-AP4_8_4,-AP4_8_5,-AP4_8_6,-AP4_5_08,-AP4_5_10,-AP6_1_1)
 
-Data_per_est <- Data %>% 
-  filter(NOM_ENT == "Ciudad de Mexico") %>%
+Data_per_est <- Data_CDMX %>% 
   group_by(NOM_MUN) %>%
-  summarise(Robo_Asalto = mean(AP7_4_05),Otro_Robo = mean(AP7_4_06),Fraude_b = mean(AP7_4_07),Fraude_con = mean(AP7_4_08),
-            Extorsion = mean(AP7_4_09),Amenaza = mean(AP7_4_10),Lesion = mean(AP7_4_11),Secuestro = mean(AP7_4_12),Host = mean(AP7_4_13),
-            Violacion = mean(AP7_4_14),Otros = mean(AP7_4_15))
+  summarise(Robo_Asalto = mean(Vic_Rob_As), Ac_Sex = mean(Vic_Sex))
 
 Data_per_est_top_n <- Data_per_est %>% top_n(n = 10, wt = Sum_Num_Inc)
 
