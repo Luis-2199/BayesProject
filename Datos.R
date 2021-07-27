a889b24b35bbe9d2255f892d9ebcfa5a315a79e4
Edson Fernández <edsonjfg98@ciencias.unam.mx>
2021-07-27 16:50:10 -0500

    Última edición

--- Datos.R
+++ Datos.R
@@ -24,31 +24,32 @@ Data <- inner_join(Data_raw_Sec3, Data_raw_Sec45) %>%
         select(-AP7_4_13, -AP7_4_14,-AP7_4_05,-AP7_4_06)
 
 Data_CDMX <- Data %>% 
-  filter(NOM_ENT == "Ciudad de Mexico") %>%
+  filter(Nom_Ent == "Ciudad de Mexico") %>%
   mutate(Sit_Lab_Act =AP3_8, Pos_OCup =replace_na( AP3_10, 0), Imp_Seg = AP4_2_05, Seg_Loc=AP4_3_1, Seg_Mun =AP4_3_2, Alum =AP4_8_1,
          Agua=AP4_8_2,Pandill=AP4_8_4,Robos=AP4_8_5,Del_Esc=AP4_8_6,Mas_Op_Del=AP4_5_08,Mas_Pat_Vil=AP4_5_10, Vehic=AP6_1_1) %>%
   select(-AP3_8,-AP3_10,-AP4_2_05,-AP4_3_1,-AP4_3_2,-AP4_8_1,-AP4_8_2,-AP4_8_4,-AP4_8_5,-AP4_8_6,-AP4_5_08,-AP4_5_10,-AP6_1_1)
 
-Data_per_est <- Data_CDMX %>% 
-  group_by(NOM_MUN) %>%
-  summarise(Robo_Asalto = mean(Vic_Rob_As), Ac_Sex = mean(Vic_Sex))
 
-Data_per_est_top_n <- Data_per_est %>% top_n(n = 10, wt = Sum_Num_Inc)
+#Data_per_est <- Data_CDMX %>% 
+#  group_by(NOM_MUN) %>%
+#  summarise(Robo_Asalto = mean(Vic_Rob_As), Ac_Sex = mean(Vic_Sex))
 
-Data_cdmx <- Data %>% 
-              filter(NOM_ENT == "Ciudad de Mexico") 
+#Data_per_est_top_n <- Data_per_est %>% top_n(n = 10, wt = Sum_Num_Inc)
+
+#Data_cdmx <- Data %>% 
+#              filter(NOM_ENT == "Ciudad de Mexico") 
               
 
-Data_Izt <- Data_cdmx %>% 
-            filter(NOM_MUN == "Iztapalapa")
+#Data_Izt <- Data_cdmx %>% 
+#            filter(NOM_MUN == "Iztapalapa")
 
 
-Data_per_mun <-  Data_cdmx %>% 
-                  group_by(NOM_MUN) %>% 
-                  summarise(Sum_Num = sum(Num_Inc))
+#Data_per_mun <-  Data_cdmx %>% 
+#                  group_by(NOM_MUN) %>% 
+#                  summarise(Sum_Num = sum(Num_Inc))
 
 
 
 
-
+save(Data_CDMX,file="BD_CDMX_Vic.Rda")
 
