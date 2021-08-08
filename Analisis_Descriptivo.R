2c4da8d4283293a5116c2d878c44cd295e289be8
luise <luis.e.pimentel@ciencias.unam.mx>
2021-08-08 14:31:29 -0500

    El cambio chido

---
+++ Analisis_Descriptivo.R
@@ -0,0 +1,133 @@
+#Analisis Descriptivo
+
+library(ggplot2)
+
+Datos_Tasas <- CData_CDMX2 %>% group_by(Region) %>% 
+  summarise(TotalDelitos = sum(Vic_Rob_As))
+
+Tasas <- rep(0,4)
+for (i in 1:4) {
+  aux1 <- CData_CDMX2 %>% filter(Region == Datos_Tasas$Region[i])
+  Tasas[i] <- length(aux1$Vic_Rob_As)
+}
+
+Tasas <- (Datos_Tasas$TotalDelitos/Tasas)*100
+
+Datos_Tasas <- tibble(Datos_Tasas, Tasas = signif(Tasas, digits = 4))
+
+# Gráfica por región # Modificar
+Datos_Tasas %>% 
+  ggplot(aes(x = "", y = Tasas, fill = Region)) +
+  geom_bar(stat = "identity", color = "White") +
+  # scale_color_hue(l = 40, c = 35) +
+  # scale_fill_hue(l = 40, c = 35) +
+  coord_polar(theta = "y")  + 
+  geom_text(aes(label = paste0(Tasas, "%")), position = position_stack(vjust=0.5)) +
+  labs(x = NULL, y = NULL, fill = NULL) +
+# theme(axis.text.x = element_text(size = 7, angle = 45))
+  scale_fill_brewer(palette = "Blues") 
+
+# Gráfica por Sexo
+CData_CDMX1 %>% group_by(Sexo) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Sexo, fill = Sexo)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica por Situación Laboral
+CData_CDMX2 %>% group_by(Sit_Lab_Act) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Sit_Lab_Act, fill = Sit_Lab_Act)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica por Nivel Academico
+CData_CDMX2 %>% group_by(Nivel_Edu) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Nivel_Edu, fill = Nivel_Edu)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica por Imp_Seg
+CData_CDMX2 %>% group_by(Imp_Seg) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Imp_Seg, fill = Imp_Seg)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica por Seg_Mun
+CData_CDMX2 %>% group_by(Seg_Mun) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Seg_Mun, fill = Seg_Mun)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica por Mas_Pat_Vil = Mas Patrullas Vigilando
+CData_CDMX2 %>% group_by(Mas_Pat_Vil) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Mas_Pat_Vil, fill = Mas_Pat_Vil)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica Vehic
+CData_CDMX2 %>% group_by(Vehic) %>% summarise(TotalDelitos = sum(Vic_Rob_As))%>%
+  ggplot(aes(x = "", y = TotalDelitos, color = Vehic, fill = Vehic)) +
+  geom_bar(stat = "identity", color = "White") +
+  coord_polar(theta = "y")
+
+# Gráfica Edad
+CData_CDMX2 %>% group_by(Edad) %>% summarise(TotalDelitos = sum(Vic_Rob_As)) %>% 
+  ggplot(aes(x = Edad, y = TotalDelitos, color = Edad, fill = Edad)) +
+  geom_bar(stat = "identity")
+
+
+
+
+
+#Scatter Plot todos los datos
+CData_CDMX %>% ggplot() + geom_point(aes(x = 1:5496,y = Vic_Rob_As))
+
+# Barplot número de delitos
+CData_CDMX %>%
+  ggplot() +
+  geom_bar(aes(x = Vic_Rob_As))
+
+#Barplot sexo
+CData_CDMX %>%
+  ggplot() +
+  geom_bar(aes(x = Sexo, y = ))
+
+
+
+
+
+
+
+ggplot(CData_CDMX) + geom_bar(aes(x= Edad, y = Vic_Rob_As), stat = "identity")
+ggplot(CData_CDMX) + geom_bar(aes(x= Edad, y = Vic_Rob_As), stat = "identity")
+
+
+ggplot(CData_CDMX) + geom_bar(aes(x = Vic_Rob_As, y = TotalDelitos))
+
+ggplot(CData_CDMX %>% filter(Nom_Mun == "Azcapotzalco")) + geom_bar(aes(x = Vic_Rob_As))
+
+ggplot(CData_CDMX %>% filter(Nom_Mun =="Coyoacan")) + geom_bar(aes(x = Vic_Rob_As))
+
+
+ggplot(CData_CDMX) + geom_bar(aes(x = Sit_Lab_Act))
+
+ggplot(CData_CDMX) + geom_bar(aes(x = Pos_OCup))
+
+ggplot(CData_CDMX) + geom_bar(aes(x = Imp_Seg))
+
+ggplot(CData_CDMX) + geom_bar(aes(x = ))
+
+
+
+
+library(maps)
+
+mi_counties <- map_data("county", "mexico") %>% 
+  select(lon = long, lat, group, id = subregion)
+head(mi_counties)
+#>     lon  lat group     id
+#> 1 -83.9 44.9     1 alcona
+#> 2 -83.4 44.9     1 alcona
+#> 3 -83.4 44.9     1 alcona
+#> 4 -83.3 44.8     1 alcona
+#> 5 -83.3 44.8     1 alcona
+#> 6 -83.3 44.8     1 alcona
--- Datos.R
+++ Datos.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+setwd("C:/Users/edson/Documents/BayesProject")
 Data_raw_Sec3 <- read_csv("Sec3.csv"#, col_types = list(col_character(), col_factor(), col_integer(),
                                                        #  col_integer(), col_integer(), col_character(),
                                                         # col_integer(), col_character(), col_integer(),
@@ -12,36 +12,44 @@ Data_raw_Sec67 <- read_csv("sec67.csv")
 
 glimpse(Data_raw_Sec67)
 
-Data_join <- inner_join(Data_raw_Sec3, Data_raw_Sec45) %>%  
-              inner_join(Data_raw_Sec67) 
+Data <- inner_join(Data_raw_Sec3, Data_raw_Sec45) %>%  
+        inner_join(Data_raw_Sec67) %>% 
+        select(-ID_PER, -CVE_ENT, -CVE_MUN, -AP6_1_2,-AP7_3_07,-AP7_3_08,-AP7_3_09,-AP7_3_10,-AP7_3_11,-AP7_3_12,-AP7_3_15,
+               -AP7_4_07,-AP7_4_08,-AP7_4_09,-AP7_4_10,-AP7_4_11,-AP7_4_12,-AP7_4_15) %>% 
+        filter(AP7_3_05 != 9,AP7_3_06 != 9,AP7_3_13 != 9,AP7_3_14 != 9) %>% 
+        mutate(AP7_4_05 = replace_na( AP7_4_05, 0), AP7_4_06 = replace_na(AP7_4_06, 0),AP7_4_13 = replace_na( AP7_4_13, 0),
+               AP7_4_14 = replace_na( AP7_4_14, 0)) %>% 
+        select(-AP7_3_05,-AP7_3_06,-AP7_3_13,-AP7_3_14) %>%
+        mutate(Vic_Rob_As = AP7_4_06 + AP7_4_05,Vic_Sex = AP7_4_13 + AP7_4_14) %>% 
+        select(-AP7_4_13, -AP7_4_14,-AP7_4_05,-AP7_4_06)
 
-Data <- Data_join %>% 
-          select(-ID_PER, -CVE_ENT, -CVE_MUN, -AP6_1_2) %>% 
-          filter(AP7_3_13 != 9, AP7_3_14 != 9) %>% 
-          mutate(AP7_4_13 = replace_na( AP7_4_13, 0), AP7_4_14 = replace_na(AP7_4_14, 0)) %>% 
-          select(-AP7_3_13, -AP7_3_14) %>%  
-          mutate(Num_Inc = AP7_4_13 + AP7_4_14) %>% 
-          select(-AP7_4_13, -AP7_4_14)
+Data_CDMX <- Data %>% 
+  filter(Nom_Ent == "Ciudad de Mexico") %>%
+  mutate(Sit_Lab_Act =AP3_8, Pos_OCup =replace_na( AP3_10, 0), Imp_Seg = AP4_2_05, Seg_Loc=AP4_3_1, Seg_Mun =AP4_3_2, Alum =AP4_8_1,
+         Agua=AP4_8_2,Pandill=AP4_8_4,Robos=AP4_8_5,Del_Esc=AP4_8_6,Mas_Op_Del=AP5_1_08,Mas_Pat_Vil=AP5_1_10, Vehic=AP6_1_1) %>%
+  select(-AP3_8,-AP3_10,-AP4_2_05,-AP4_3_1,-AP4_3_2,-AP4_8_1,-AP4_8_2,-AP4_8_4,-AP4_8_5,-AP4_8_6,-AP5_1_08,-AP5_1_10,-AP6_1_1)
 
-Data_per_est <- Data %>% 
-                  group_by(NOM_ENT) %>% 
-                  summarise(Sum_Num_Inc = sum(Num_Inc))
 
-Data_per_est_top_n <- Data_per_est %>% top_n(n = 10, wt = Sum_Num_Inc)
+#Data_per_est <- Data_CDMX %>% 
+#  group_by(NOM_MUN) %>%
+#  summarise(Robo_Asalto = mean(Vic_Rob_As), Ac_Sex = mean(Vic_Sex))
 
-Data_cdmx <- Data %>% 
-              filter(NOM_ENT == "Ciudad de Mexico") 
+#Data_per_est_top_n <- Data_per_est %>% top_n(n = 10, wt = Sum_Num_Inc)
+
+#Data_cdmx <- Data %>% 
+#              filter(NOM_ENT == "Ciudad de Mexico") 
               
 
-Data_Izt <- Data_cdmx %>% 
-            filter(NOM_MUN == "Iztapalapa")
+#Data_Izt <- Data_cdmx %>% 
+#            filter(NOM_MUN == "Iztapalapa")
 
 
-Data_per_mun <-  Data_cdmx %>% 
-                  group_by(NOM_MUN) %>% 
-                  summarise(Sum_Num = sum(Num_Inc))
+#Data_per_mun <-  Data_cdmx %>% 
+#                  group_by(NOM_MUN) %>% 
+#                  summarise(Sum_Num = sum(Num_Inc))
 
 
 
 
+save(Data_CDMX,file="BD_CDMX_Vic.Rda")
 
