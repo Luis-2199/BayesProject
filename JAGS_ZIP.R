044d7f2c0f4113fa6028d00e46a01ef7f097b307
luise <luis.e.pimentel@ciencias.unam.mx>
2021-08-12 03:44:46 -0500

    JAGS casi para ZIP

--- JAGS_ZIP.R
+++ JAGS_ZIP.R
@@ -8,17 +8,21 @@ mod_dist_4 <- zeroinfl(Vic_Rob_As ~ Seg_Mun  + Region |
                        data= CData_CDMX2, dist="poisson",link="logit")
 summary(mod_dist_4)
 
-p <- predict(mod_dist_4, type = "zero")
-lambda <- predict(mod_dist_4, type = "count")
+mnull <- update(mod_dist_4, . ~ 1)
 
-sim <- rzipois(1000, lambda[359], p[359])
-sim <- rpois(1000, lambda[359])
+pchisq(2*(logLik(mod_dist_4) - logLik(mnull)), df = 6, lower.tail = FALSE) # Es estadísticamente significativo que el poisson sin covariables 
+
+summary(mod_pois_4 <- glm(Vic_Rob_As ~ Seg_Mun + Region, family = poisson, data = CData_CDMX2))
+
+vuong(mod_dist_4, mod_pois_4) # Si es mejor aplicar un Poisson inflado
+
+
 
 #### JAGS
 attach(CData_CDMX2)
 
 data <- list(
-  y = as.numeric(Vic_Rob_As),
+  y = Vic_Rob_As,
   X1 = Edad,
   X2 = Mas_Pat_Vil,
   X3 = as.numeric(Region),
@@ -35,12 +39,13 @@ inits <- function(){ list(
 )
 }
 
-modelo = "model{
+###### Con este sí convergen, pero difieren mucho los frecuentistas con el Bayesiano
+modelo1 = "model{
 for(i in 1:n){
 
 y[i] ~ dpois(mu[i])
-mu[i] <- (u[i]+0.000001)*lambda[i]
-u[i] ~ dbern(1-p[i])
+mu[i] <- (1-u[i]+0.0000001)*lambda[i]
+u[i] ~ dbern(p[i])
 
 logit(p[i]) <- alpha0 + alpha1*X1[i] + alpha2[X2[i]] + alpha3[X3[i]] + alpha4[X4[i]] 
 
@@ -77,11 +82,67 @@ beta2[4] ~ dnorm(0.0, 0.0001)
 
 }
 "
-fit <- jags.model(textConnection(modelo), data, inits, n.chains = 2)
-update(fit, 5000)
+fit1 <- jags.model(textConnection(modelo1), data, inits, n.chains = 2)
+update(fit1, 5000)
 
-sample <- coda.samples(fit, param, n.iter = 5000, thin = 1)
+sample1 <- coda.samples(fit1, param, n.iter = 5000, thin = 1)
 
-plot(sample)
-gelman.plot(sample)
-summary(sample)
+plot(sample1)
+gelman.plot(sample1)
+summary(sample1)
+######
+
+######## como Viene en el libro (Creo que no convergen )
+modelo2 = "model{
+for(i in 1:n){
+
+y[i] ~ dpois(mu[i])
+mu[i] <- (u[i] + 0.00000001)*lambda[i]
+u[i] ~ dbern(p[i])
+
+logit(p[i]) <- alpha0 + alpha1*X1[i] + alpha2[X2[i]] + alpha3[X3[i]] + alpha4[X4[i]] 
+
+log(lambda[i]) <- beta0 + beta1[X5[i]] + beta2[X3[i]]
+
+zdp[i] <- (1 - p[i]) + p[i]*exp(-lambda[i])}
+
+#### Priors
+
+alpha0 ~ dnorm(0.0, 0.0001)
+beta0 ~ dnorm(0.0, 0.0001)
+
+alpha1 ~ dnorm(0.0, 0.0001)
+
+alpha2[1] <- 0
+alpha2[2] ~ dnorm(0.0, 0.0001)
+
+alpha3[1] <- 0
+alpha3[2] ~ dnorm(0.0, 0.0001)
+alpha3[3] ~ dnorm(0.0, 0.0001)
+alpha3[4] ~ dnorm(0.0, 0.0001)
+
+alpha4[1] <- 0
+alpha4[2] ~ dnorm(0.0, 0.0001)
+alpha4[3] ~ dnorm(0.0, 0.0001)
+
+beta1[1] <- 0
+beta1[2] ~ dnorm(0.0, 0.0001)
+
+beta2[1] <- 0
+beta2[2] ~ dnorm(0.0, 0.0001)
+beta2[3] ~ dnorm(0.0, 0.0001)
+beta2[4] ~ dnorm(0.0, 0.0001)
+
+}
+"
+
+
+fit2 <- jags.model(textConnection(modelo2), data, inits, n.chains = 2)
+update(fit2, 5000)
+
+set.seed(1)
+sample2 <- coda.samples(fit2, param, n.iter = 5000, thin = 1)
+
+plot(sample2)
+gelman.plot(sample2)
+summary(sample2)
